08bc0c6d029c6c851525c4a29a56bf5b98f89883
ivan <Sakhman.I@gmail.com>
2019-08-15 23:59:26 +0300

    add sign in

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -30,7 +30,7 @@ class SignUp extends Component {
         password
       );
 
-      createUserProfileDocument(user, { displayName });
+      await createUserProfileDocument(user, { displayName });
 
       this.setState({
         displayName: "",
